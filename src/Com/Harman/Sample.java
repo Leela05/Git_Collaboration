d061efd07a43d93dee94f6f64a0e0e476efd626c
Leela05 <leelarenu05@gmail.com>
2022-03-19 18:50:29 +0530

    Addition

--- src/Com/Harman/Sample.java
+++ src/Com/Harman/Sample.java
@@ -1,8 +1,18 @@
 package Com.Harman;
 
+import java.util.Scanner;
+
 public class Sample {
     public static void main(String[] args) {
-        
+        int number1, number2, number3;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the 1st number:");
+        number1=sc.nextInt();
+        System.out.println("Enter the 2nd number:");
+        number2=sc.nextInt();
+        number3 = number1 + number2;
+        System.out.println("Addition of two numbers:"+number3);
+
     }
 
 }
